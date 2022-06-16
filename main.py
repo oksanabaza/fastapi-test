6eb5dd616c4b53ad48031c1f8dbe903ffb0b169a
oksanamisiura <oksana.misiura.cld@gmail.com>
2022-06-16 12:40:26 +0100

    cors added

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins="http://localhost:3000",
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user)
